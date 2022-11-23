8b1e550429429372c36d8d4709d47714a35ac72d
Veronika Smoliak <a.smolyak@cardsmobile.ru>
2022-11-24 00:02:58 +0300

    task2 added

---
+++ task2/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine ("Введите значение b1: ");
+double b1 = double.Parse (Console.ReadLine ()!);
+Console.WriteLine ("Введите значение k1: ");
+double k1 = double.Parse (Console.ReadLine ()!);
+Console.WriteLine ("Введите значение b2: ");
+double b2 = double.Parse (Console.ReadLine ()!);
+Console.WriteLine ("Введите значение k2: ");
+double k2 = double.Parse (Console.ReadLine ()!);
+
+double x = findX (b1, k1, b2, k2);
+double y = findY (x, k1, b1);
+
+
+double findX (double b1, double k1, double b2, double k2) {
+
+    double x = (b2 - b1) / (k1 - k2);
+    return x;
+}
+
+double findY (double x, double k1, double b1) {
+
+    double y = k1 * x + b1;
+    Console.WriteLine ("x = " + x + " y = " + y);
+    return y;
+}
+
